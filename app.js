8a22f07e00c63575a277f378d6af19ec5c68d0c2
NicoExcalibur <nico.zanz@gmail.com>
2021-06-04 12:12:45 +0200

    pause function

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 // get elements
 const player = document.querySelector(".player");
-const video = plyer.querySelector(".viewer");
+const video = player.querySelector(".viewer");
 const progress = player.querySelector(".progress");
 const progressBar = player.querySelector(".progress__filled");
 
-const togglr = player.querySelector(".toggle");
+const toggle = player.querySelector(".toggle");
 const skipButtons = player.querySelectorAll("[data-skip]");
 const ranges = player.querySelectorAll(".player__slider");
 
 // build functions
+function togglePlay() {
+  const method = video.paused ? "play" : "pause";
+  video[method]();
+}
 
 // hook up event listeners
+video.addEventListener("click", togglePlay);
+toggle.addEventListener("click", togglePlay);
