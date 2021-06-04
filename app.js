e303f792aba48be67855cece8c96cd23c4c0546f
NicoExcalibur <nico.zanz@gmail.com>
2021-06-04 11:58:19 +0200

    get elements

---
+++ app.js
@@ -0,0 +1,13 @@
+// get elements
+const player = document.querySelector(".player");
+const video = plyer.querySelector(".viewer");
+const progress = player.querySelector(".progress");
+const progressBar = player.querySelector(".progress__filled");
+
+const togglr = player.querySelector(".toggle");
+const skipButtons = player.querySelectorAll("[data-skip]");
+const ranges = player.querySelectorAll(".player__slider");
+
+// build functions
+
+// hook up event listeners
