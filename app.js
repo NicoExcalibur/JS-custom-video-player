ccc676f66a28bceca422a4c5e992a0be9cd661b6
NicoExcalibur <nico.zanz@gmail.com>
2021-06-04 12:38:44 +0200

    update button at pause and read

--- app.js
+++ app.js
@@ -14,6 +14,15 @@ function togglePlay() {
   video[method]();
 }
 
+function updateButton() {
+  const icon = this.paused ? "►" : "❚❚";
+  console.log(icon);
+  toggle.textContent = icon;
+}
+
 // hook up event listeners
 video.addEventListener("click", togglePlay);
+video.addEventListener("play", updateButton);
+video.addEventListener("pause", updateButton);
+
 toggle.addEventListener("click", togglePlay);
